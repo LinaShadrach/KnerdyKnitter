a84a2e458a5c50c551838a14342f3674361cd4f5
Lina Shadrach <lina.shadrach@gmail.com>
2017-03-06 11:34:17 -0800

    add register, login, and logout logic and views

--- src/KnerdyKnitter/Controllers/AccountController.cs
+++ src/KnerdyKnitter/Controllers/AccountController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using KnerdyKnitter.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using KnerdyKnitter.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +14,99 @@ namespace KnerdyKnitter.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: /<controller>/
+        private KnerdyKnitterContext db = new KnerdyKnitterContext();
+
+
+        //Basic User Account Info here...
+        private readonly KnerdyKnitterContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, KnerdyKnitterContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            { 
+                string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var thisWorker = db.Knitters.FirstOrDefault(knitter => knitter.ApplicationUserId == userId);
+                return View(thisWorker);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Register()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            int index = Array.IndexOf(model.Email.ToArray(), '@');
+            ApplicationUser user = new ApplicationUser { UserName = model.Email.Substring(0,index), Email = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                //Knitter newKnitter = new Knitter();
+                //newKnitter.SignUpDate = DateTime.Now;
+
+                return await RegisterLogin(user, model.Password);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterLogin(ApplicationUser user, string password)
+        {
+
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user.UserName, password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+
+                return RedirectToAction("Index", "Account");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
         }
     }
 }
