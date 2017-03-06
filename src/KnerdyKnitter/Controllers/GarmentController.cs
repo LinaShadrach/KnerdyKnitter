b4c15c793abbd8b4ef4e63daec7da8349c8bc1be
Lina Shadrach <lina.shadrach@gmail.com>
2017-03-06 15:46:27 -0800

    add create garment form; add logic for saving garment to db

---
+++ src/KnerdyKnitter/Controllers/GarmentController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KnerdyKnitter.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KnerdyKnitter.Controllers
+{
+    public class GarmentController : Controller
+    {
+        private readonly KnerdyKnitterContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public GarmentController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, KnerdyKnitterContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Create()
+        {
+            Garment emptyGarment = new Garment();
+            return View(emptyGarment);
+        }
+        [HttpPost]
+        public IActionResult Create(Garment garment)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentKnitter = _db.Knitters.FirstOrDefault(k => k.UserId == userId);
+            garment.KnitterId = currentKnitter.Id;
+            garment.CreationDate = DateTime.Now;
+            garment.Save(garment);
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            Garment thisGarment = _db.Garments.FirstOrDefault(g => g.KnitterId == id);
+            return View(thisGarment);
+        }
+        [HttpPost]
+        public IActionResult Edit(Garment garment)
+        {
+            garment.Edit(garment);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Garment thisGarment = _db.Garments.FirstOrDefault(g => g.KnitterId == id);
+            thisGarment.Remove(thisGarment);
+            return View();
+        }
+    }
+}
--- src/KnerdyKnitter/Models/Garment.cs
+++ src/KnerdyKnitter/Models/Garment.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KnerdyKnitter.Models
 {
@@ -24,6 +23,40 @@ namespace KnerdyKnitter.Models
         public virtual List<Alters> Alters { get; set; }
         public virtual List<GarmentTag> GarmentTags { get; set; }
         public virtual List<Favorite> Favorites { get; set; }
-        public virtual List<Comment> Comments { get; set; } 
+        public virtual List<Comment> Comments { get; set; }
+
+        KnerdyKnitterContext db = new KnerdyKnitterContext();
+        public Garment()
+        {
+            RowDim = 100;
+            ColDim = 10;
+            Rule = "30";
+            CreationDate = DateTime.Now;
+        }
+        public Garment Save(Garment garment)
+        {
+            db.Garments.Add(garment);
+            db.SaveChanges();
+            return garment;
+        }
+
+        public Garment Edit(Garment garment)
+        {
+            db.Entry(garment).State = EntityState.Modified;
+            db.SaveChanges();
+            return garment;
+        }
+
+        public void Remove(Garment garment)
+        {
+            db.Garments.Remove(garment);
+            db.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            db.RemoveRange(db.Garments);
+            db.SaveChanges();
+        }
     }
 }
