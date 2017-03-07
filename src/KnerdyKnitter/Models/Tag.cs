c4307a87f45957a4dbc900e36859e3243c75a963
Lina Shadrach <lina.shadrach@gmail.com>
2017-03-06 17:02:23 -0800

    add crud for tags

--- src/KnerdyKnitter/Models/Tag.cs
+++ src/KnerdyKnitter/Models/Tag.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,5 +15,31 @@ namespace KnerdyKnitter.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual List<GarmentTag> GarmentTags { get; set; }
+
+        KnerdyKnitterContext db = new KnerdyKnitterContext();
+
+        public Tag Save(Tag garment)
+        {
+            db.Tags.Add(garment);
+            db.SaveChanges();
+            return garment;
+        }
+        public Tag Edit(Tag garment)
+        {
+            db.Entry(garment).State = EntityState.Modified;
+            db.SaveChanges();
+            return garment;
+        }
+        public void Remove(Tag garment)
+        {
+            db.Tags.Remove(garment);
+            db.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            db.RemoveRange(db.Tags);
+            db.SaveChanges();
+        }
     }
 }
